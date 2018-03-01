61b5aae7f40396e9e737af360a7727627a9434a0
David Gamero <david340804@gmail.com>
2018-03-01 02:11:27 -0500

    Shuffling function

--- laya.js
+++ laya.js
@@ -1,5 +1,7 @@
 console.log('leggo bitches');
 
+words = ['letters','aggregate'];
+
 //fetch('http://143.215.110.228:8080/d/f/100');
 
 /**
@@ -22,6 +24,29 @@ function remapWord(word,newLetters){
 }
 
 /**
+	Reorder an array randomly
+	https://bost.ocks.org/mike/shuffle/
+*/
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+/**
 	Jumble up a word
 */
 function jumble(word){
@@ -33,11 +58,6 @@ function jumble(word){
 	lastLetter = word[word.length - 1];
 
 	scrambled = '';
-	remainingIndices = [];
-
-	for(i = 0; i<word.length; i++){
-		remainingIndices.push(i);
-	}
 
 	newFirst = Math.floor(Math.random()*(word.length-2)) + 1;
 
